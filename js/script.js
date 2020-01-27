34b2e87134c56889f69630a410c7ccbbbdff72fd
Emanuele <emanuele.correggia86@gmail.com>
2020-01-27 17:04:21 +0100

    invio mess js riuscito SIA LODATO GESU' CRISTO

--- js/script.js
+++ js/script.js
@@ -23,3 +23,15 @@ $(document).ready(function () {
     });
   });
 });
+// funzione che invia messaggio utente
+function sendMessage() {
+  var textMessage = $('.second_item input.send-message').val();
+    if(textMessage.length != 0) {
+      var newMessage = $('.template .message').clone();
+      console.log(newMessage);
+      newMessage.find('.message-text').text(textMessage);
+      newMessage.addClass('sent');
+      $('.col-right-messages.active').append(newMessage);
+
+  }
+}
