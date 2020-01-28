7ec0700737151b69386d4c57907fb6bcbaa779a0
Emanuele <emanuele.correggia86@gmail.com>
2020-01-28 19:08:19 +0100

    fine manca indendazione

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
-  $('.last_item').click(function () {
+  $('.icon-send').click(function () {
     sendMessage();
   });
+
   $('.send-message').keypress(function(event) {
     if(event.which == 13) {
       sendMessage();
     }
   });
-  $('.search_chat input').keyup(function () {
-    var text = $('.search_chat input').val().toLowerCase();
-    $('.chat').each(function () {
-      var contactName = $(this).find('.nome').text().toLowerCase();
+
+  $('.contact-search input').keyup(function () {
+    var text = $('.contact-search input').val().toLowerCase();
+
+    $('.contact-element').each(function () {
+      var contactName = $(this).find('.contact-name').text().toLowerCase();
       if(contactName.includes(text) == true) {
         $(this).show();
       } else {
@@ -18,44 +21,98 @@ $(document).ready(function () {
       }
     });
   });
+
+
+
+  // mostra e nasconde i dropdown
+  $(document).on('click', '.message-options', function() {
+    $(this).parent().siblings('.message-link').toggleClass('active');
+    $(this).parents('.message').siblings('.message').find('.message-link').removeClass('active');
+  });
+
+
+  $(document).on('click', '.message-delete', function() {
+    $(this).parents('.message').remove();
+  });
+
+  $(document).on('click', '.contact-element', function() {
+    var data = $(this).attr('data-contact');
+    var selector = '.col-right-messages[data-contact="' + data + '"]';
+
+    $('.col-right-messages').removeClass('active');
+    $(selector).addClass('active');
+    $('.contact-element').removeClass('active');
+    $(this).addClass('active');
+
+    var name = $(this).find('.contact-name').text();
+    var time = $(this).find('.contact-time').text();
+    var img = $(this).find('.avatar img').attr('src');
+    $('.col-right .header .contact-active .contact-name').text(name);
+    $('.col-right .header .contact-active .contact-time').text(time);
+    $('.col-right .header .avatar img').attr('src', img);
+  });
+
+  $('.send-message').focus(function(){
+    $('.icon-send i').removeClass('fa fa-microphone').addClass('fas fa-paper-plane');
+  }).blur(function(){
+    $('.icon-send i').removeClass('fas fa-paper-plane').addClass('fa fa-microphone');
+  });
+
 });
+
+
+// ------------- FUNZIONI -------------//
+
 // funzione che invia messaggio utente
 function sendMessage() {
-  var textMessage = $('.second_item input.send-message').val();
+  var textMessage = $('input.send-message').val();
+
   if(textMessage.length != 0) {
     var newMessage = $('.template .message').clone();
     console.log(newMessage);
+
     newMessage.find('.message-text').text(textMessage);
+
     var data = new Date();
     var hours = addZero(data.getHours());
     var minutes = addZero(data.getMinutes());
     var time = hours +':'+ minutes;
+
     newMessage.find('.message-time').text(time);
     newMessage.addClass('sent');
     $('.col-right-messages.active').append(newMessage);
     scrollMessage();
     setTimeout(sendResponse, 1000);
+
     $('input.send-message').val('');
   }
 }
+
 // funzione che manda risposta
 function sendResponse() {
   var messageResponse = $('.template .message').clone();
   messageResponse.find('.message-text').text('ok');
+
   var data = new Date();
   var hours = addZero(data.getHours());
   var minutes = addZero(data.getMinutes());
   var time = hours +':'+ minutes;
   messageResponse.find('.message-time').text(time);
+
   messageResponse.addClass('received');
   $('.col-right-messages.active').append(messageResponse);
   scrollMessage();
 }
+
 // funzione che scrolla
 function scrollMessage() {
-    var heightContainer = $('.col-right-messages.active').height();
-    $('.col-right-messages.active').scrollTop(heightContainer);
+   // altezza elemento conversazione attiva
+  var heightContainer = $('.col-right-messages.active').height();
+  console.log(heightContainer);
+  // spostiamo scroll container di tutte le conversazioni
+  $('.messages-wrapper').scrollTop(heightContainer);
 }
+
 // Funzione che aggiunge zero
 function addZero(number) {
   if(number < 10) {
@@ -63,19 +120,3 @@ function addZero(number) {
   }
   return number;
 }
-// intercetto il focus e trasformo l'icona del microfono in invio
-$('.second_item input').focus(function(){
-  $('.last_item .send i').toggleClass('fas fa-microphone fas fa-paper-plane');
-}).blur(function(){
-    $('.send i').toggleClass('fas fa-microphone fa fas fa-paper-plane')
-});
-
-//  al click di 'Cancella Messaggio', questo, viene rimosso dal DOM
-$(document).on('click', '.delete_message', function() {
-  $(this).parents('.message').remove();
-});
-
-//  cliccando sulla chevron nel messaggio, spunta il dropdown
-$(document).on('click', '.fa fa-chevron-down', function() {
-  $(this).siblings('.menu').toggleClass('menu_on');
-});
